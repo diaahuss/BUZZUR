3a44d6a2b7e4e04356e69f9feea66c1be1e95c9b
diaa hussein <diaahussein110@gmail.com>
2025-04-12 20:53:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const cors = require('cors');
+const path = require('path');
+const { Server } = require('socket.io');
+
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
 
-app.use(express.static('public'));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
 io.on('connection', (socket) => {
   console.log('A user connected');
-  
-  socket.on('buzz', (data) => {
-    console.log('Buzz event received:', data);
-    // Emit buzz to all clients in the group
-    io.emit('buzz', data); 
+
+  socket.on('buzz', ({ members }) => {
+    console.log('Buzz sent to:', members.map(m => m.name).join(', '));
+    io.emit('buzz'); // broadcast buzz to all
   });
 
   socket.on('disconnect', () => {
-    console.log('User disconnected');
+    console.log('A user disconnected');
   });
 });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running');
-});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
